f9375bc84b6dc9bd957c1cb2e0fea22802769142
JH <dafr47@naver.com>
2023-08-28 15:59:50 +0900

    자동차 실습문제 추가

---
+++ src/자동차만들기실습문제/Bus.java
@@ -0,0 +1,12 @@
+package 자동차만들기실습문제;
+
+public class Bus extends  Car{
+    Bus(int speed, int fuelEfficiency, int seat, String name, int fuelCost, int fuelTankSize){
+        this.speed = speed;
+        this.fuelEfficiency = fuelEfficiency;
+        this.ableSeat = seat;
+        this.name = name;
+        this.fuelCost = fuelCost;
+        this.fuelTankSize = fuelTankSize;
+    }
+}
