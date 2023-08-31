a7f261b912d05231f254f2e0eb26ac6a9081b0c8
JH <dafr47@naver.com>
2023-08-31 17:39:26 +0900

    학생 정렬 문제 + 단어 정렬 디테일 추가

--- src/단어정렬/WordSortEx.java
+++ src/단어정렬/WordSortEx.java
@@ -5,9 +5,7 @@ package 단어정렬;
 // 2. 길이가 같으면 사전 순으로
 // 3. 중복 제거
 
-import java.util.Comparator;
-import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.*;
 
 public class WordSortEx {
     public static void main(String[] args) {
@@ -23,9 +21,27 @@ public class WordSortEx {
         });
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        String[] word = new String[n];
         for (int i = 0; i < n; i++){
             ts.add(sc.next());
         }
+        for (int i =0; i < n; i++) word[i] = sc.next();
+        HashSet<String> set = new HashSet<>(Arrays.asList(word));
+        word = set.toArray(new String[0]);
+        Arrays.sort(word, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                }
+                else {
+                    return o1.length() - o2.length();
+                }
+            }
+        });
+
         for (String e : ts) System.out.print(e + " ");
+        System.out.println(Arrays.toString(word));
+
     }
 }
