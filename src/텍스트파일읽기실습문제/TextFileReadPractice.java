c042113cc48af9a33559f97d4a16c131185c03b0
JH <dafr47@naver.com>
2023-09-05 22:38:41 +0900

    텍스트 파일 읽기 풀이 추가

--- src/텍스트파일읽기실습문제/TextFileReadPractice.java
+++ src/텍스트파일읽기실습문제/TextFileReadPractice.java
@@ -1,51 +1,56 @@
 package 텍스트파일읽기실습문제;
 
+import javax.swing.text.Style;
+import java.awt.*;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.*;
+import java.util.List;
 
 public class TextFileReadPractice {
     public static void main(String[] args) {
+
         try{
-            FileInputStream fis = new FileInputStream("d:/temp/StudentGrade.txt");
+            FileInputStream fis = new FileInputStream("src/텍스트파일읽기실습문제/StudentGrade.txt");
             BufferedInputStream bis = new BufferedInputStream(fis);
-            TreeSet<Integer> set = new TreeSet<>();
-            HashMap<String, Integer> map = new HashMap<>();
             Scanner sc = new Scanner(bis);
+            HashMap<String, Integer> map = new HashMap<>();
+            List<Integer> set = new ArrayList<>();
+            List<Integer> get = new ArrayList<>();
             String[] lines;
-            int[] sum = new int[10];
+            int sum = 0;
+            int cnt = 2;    // 처음 과목수
             int i = 0;
 
             while (sc.hasNext()){
                 String line = sc.nextLine();    // 문자엻 줄바꿈 기준으로 읽기
-                //System.out.println(line);
                 lines = line.split(" ");
                 for (String e : lines){
-                    if (isNumeric(e)){
-                        sum[i] += Integer.parseInt(e);  // 0
+                    if (isNumeric(e))
                         set.add(Integer.parseInt(e));
-                    }
-                    else if (!isNumeric(e)){
-                        map.put(e,sum[i]);
-                        if (set.size() >= 2) {
-                            i++;
-
-                            set.clear();
-                        }
-                    }
                 }
+                while (true) {
+                    sum += set.get(i);
+                    if (i == cnt){
+                        get.add(sum);
+                        map.put(lines[0], sum);
+                        cnt += 3;
+                        sum = 0;
+                    }
+                    i++;
+                    if (get.size() == set.size() / 3) break;
+                }
+
             }
-            List<Map.Entry<String, Integer>> entryList = new ArrayList<>(map.entrySet());
-            Collections.sort(entryList, new Comparator<Map.Entry<String, Integer>>() {
-                @Override
-                public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                    return o2.getValue().compareTo(o1.getValue());
-                }
-            });
-            System.out.println(entryList);
-
-
+            List<String> listKeySet = new ArrayList<>(map.keySet());
+            Collections.sort(listKeySet, (value1, value2) -> (map.get(value2).compareTo(map.get(value1))));
+            get.sort(Collections.reverseOrder());
+            System.out.println("=".repeat(5) + "내림 차순 정렬" + "=".repeat(5));
+            for (int j = 0; j < 10; j++){
+                System.out.println("[이름]" + listKeySet.get(j) + " " + "[총 점수]" + get.get(j));
+            }
 
         } catch (IOException e) {
             System.out.println("파일이 없습니다.");
