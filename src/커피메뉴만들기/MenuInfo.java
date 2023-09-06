d176de636977b13e1a2c7768a634dc3d32179c13
JH <dafr47@naver.com>
2023-09-06 09:05:35 +0900

    메뉴 역직렬화 해서 리스트 출력 문제 추가

--- src/커피메뉴만들기/MenuInfo.java
+++ src/커피메뉴만들기/MenuInfo.java
@@ -6,6 +6,10 @@ public class MenuInfo {
     String category;// 분류
     String description; // 메뉴에 대한 설명
 
+    public String getCategory() {
+        return category;
+    }
+
     public MenuInfo(String name, int price, String category, String description) {
         this.name = name;
         this.price = price;
