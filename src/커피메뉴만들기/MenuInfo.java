f7f981f5822a3d5086d3958ae351c6a4bf62e641
JH <dafr47@naver.com>
2023-09-06 09:11:11 +0900

    메뉴 수정 추가

--- src/커피메뉴만들기/MenuInfo.java
+++ src/커피메뉴만들기/MenuInfo.java
@@ -10,6 +10,18 @@ public class MenuInfo {
         return category;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
     public MenuInfo(String name, int price, String category, String description) {
         this.name = name;
         this.price = price;
